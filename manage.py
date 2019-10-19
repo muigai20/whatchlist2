8c692713e1248094ac4ec2b6ab03b9f520a2b246
Joseph Kamau <muigaijosephkamau@gmail.com>
2019-10-19 15:33:38 +0300

    updated the app

---
+++ manage.py
@@ -0,0 +1,19 @@
+from app import create_app
+from flask_script import Manager,Shell,Server
+
+# Creating app instance
+app = create_app('development')
+
+manage = Manager(app)
+
+manage.add_command('server',Server)
+
+@manage.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == '__main__':
+    manage.run()
---
+++ start.sh
@@ -0,0 +1,4 @@
+ export MOVIE_API_KEY='f26d021f1c3c7c87db43fe619e103c31'
+export SECRET_KEY='josewise'
+
+python3.6 manage.py server
